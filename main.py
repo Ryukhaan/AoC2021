699c9fa445461b43846f826244370508fa231701
Rémi Decelle <remi.decelle@loria.fr>
2021-12-06 21:10:12 +0100

    update

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def make_markdown(**kwargs):
 									"", '^', space_col,
 									"Results", "^", space_col,
 									"", "^", space_col))
-	text.append("|:-----:|----------|:--------:|-----------|")
+	text.append("|-----:|:----------:|:--------:|-----------:|")
 	text.append( row_string.format("", "^", space_col,
 		"Puzzle 1", "^", space_col,
 		"Puzzle 2", "^", space_col,
 		"Time (ms)", "^", space_col))
-	
+
 	for idx, (key, value) in enumerate(kwargs.items()):
 		day_number = key.split('_')[1]
 		day_str = upsidedown.transform("Day " + day_number)
